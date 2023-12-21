8a2e1c9012ec1f764cf446fa4e90c4c856c11ca6
rasiya <rasiyajishar1@gmail.com>
2023-12-21 11:36:24 +0530

    useParams hook added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, Link, useParams } from 'react-router-dom';
+import { User } from './user';
+function App() {
+  // const [pincodedata, setPincodeData] = useState([]);
 
-
-export default function App() {
-  const [pincodedata, setPincodeData] = useState([]);
-
-  useEffect(() => {
-    const url = "https://api.postalpincode.in/pincode/679511";
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setPincodeData(data[0]?.PostOffice || []))
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   const url = "https://api.postalpincode.in/pincode/679511";
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((data) => setPincodeData(data[0].PostOffice))
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }, []);
 
   return (
-    <div className="App">
-      <h1>Pincode Data:</h1>
-      {pincodedata.length > 0 ? (
-        <ul>
-          {pincodedata.map((item, index) => (
-            <li key={index}>{JSON.stringify(item)}</li>
-          ))}
+    <div className="App"> 
+      {/* <ul>
+        {pincodedata.map((item, index) => (
+          <li key={index}>{JSON.stringify(item.Name)}</li>
+        ))}
+      </ul> */}
+   
+
+
+    
+      <Routes>
+     
+        
+        {/* Define a route with a parameter */}
+        <Route path="/user/:id" component={User} />
+
+     
+      
+    </Routes>
+       {/* Create links to navigate to different user profiles */}
+       <ul>
+          <li><Link to="/user/1">User 1</Link></li>
+          <li><Link to="/user/2">User 2</Link></li>
+          <li><Link to="/user/3">User 3</Link></li>
         </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
     </div>
   );
-  
 }
+
+export default App;
